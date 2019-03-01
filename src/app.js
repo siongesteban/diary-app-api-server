a3adeac50a5ec4698ea25bfb4988b37d874792a3
siongesteban <siong.esteban@gmail.com>
2019-03-01 10:06:30 +0800

    Use necessary express middlewares

--- src/app.js
+++ src/app.js
@@ -1,5 +1,32 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(compression());
+
+const whitelist = ['localhost'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+const configuredCors = cors(corsOptions);
+
+app.use(configuredCors);
+app.options('*', configuredCors);
+
+app.use(helmet());
+app.use(morgan('combined'));
 
 export default app;
