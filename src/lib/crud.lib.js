166a218289da1fffbde78601689f690e74011ded
siongesteban <siong.esteban@gmail.com>
2019-03-01 18:49:53 +0800

    Create initial CRUD lib

---
+++ src/lib/crud.lib.js
@@ -0,0 +1,1 @@
+export const create = (Model, data) => Model.create(data);
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,5 @@
 import logger from './logger.lib';
 
+export * from './crud.lib';
 export * from './error.lib';
 export { logger };
