0e6b88857c8b79b72935363b78ca2311c007d19f
siongesteban <siong.esteban@gmail.com>
2019-03-02 00:02:39 +0800

    Create getDoc and use for throwing NotFoundError

--- src/lib/crud.lib.js
+++ src/lib/crud.lib.js
@@ -1,10 +1,22 @@
 import forEach from 'lodash/forEach';
+import { NotFoundError } from './error.lib';
 
 export const findDocs = (Model, params) => Model.find(params);
 
+export const getDoc = async (Model, id) => {
+  const doc = await Model.findById(id);
+  const { constructor: { modelName } } = new Model({});
+
+  if (!doc) {
+    throw new NotFoundError(modelName);
+  }
+
+  return doc;
+};
+
 export const createDoc = (Model, data) => Model.create(data);
 
-export const patchDoc = (Model, id, data) => {
+export const patchDoc = async (Model, id, data) => {
   const fieldsToPath = {};
 
   forEach(data, (value, field) => {
@@ -13,7 +25,13 @@ export const patchDoc = (Model, id, data) => {
     }
   });
 
+  await getDoc(Model, id);
+
   return Model.findByIdAndUpdate(id, { $set: fieldsToPath }, { new: true });
 };
 
-export const deleteDoc = (Model, id) => Model.findByIdAndDelete(id);
+export const deleteDoc = async (Model, id) => {
+  await getDoc(Model, id);
+
+  return Model.findByIdAndDelete(id);
+};
