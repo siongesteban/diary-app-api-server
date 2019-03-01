d3a8e6e160cba83b8114df03cde1545083bc8e9e
siongesteban <siong.esteban@gmail.com>
2019-03-01 18:49:21 +0800

    Create entry model

---
+++ src/models/entry.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+import { getMinMessage, getMaxMessage, getRequiredMessage } from '../utils';
+
+const entrySchema = new Schema(
+  {
+    title: {
+      type: String,
+      maxlength: [100, getMaxMessage('Title', 100)],
+      minlength: [3, getMinMessage('Title', 3)],
+      required: [true, getRequiredMessage('Title')],
+      unique: true,
+    },
+    content: {
+      type: String,
+      required: [true, getRequiredMessage('Content')],
+    },
+    type: {
+      type: String,
+      enum: ['academic', 'fitness', 'health', 'religious', 'travel', 'work'],
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+entrySchema.plugin(uniqueValidator);
+
+export default mongoose.model('Entry', entrySchema);
--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
+import Entry from './entry.model';
 import User from './user.model';
 
-export { User };
+export { Entry, User };
