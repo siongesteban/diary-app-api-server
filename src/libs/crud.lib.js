527c73b3add806aebf04c14c05ab581fb81f4df7
siongesteban <siong.esteban@gmail.com>
2019-03-13 02:14:59 +0800

    Use chainPopulate in CRUD methods

--- src/libs/crud.lib.js
+++ src/libs/crud.lib.js
@@ -1,12 +1,13 @@
 import forEach from 'lodash/forEach';
 
 import { NotFoundError } from './error.lib';
-import { getModelName } from '../utils';
+import { chainPopulate, getModelName } from '../utils';
 
-export const findDocs = (Model, params) => Model.find(params);
+export const findDocs = (Model, params) =>
+  chainPopulate(Model.find(params), Model);
 
 export const getDoc = async (Model, id, params) => {
-  const doc = await Model.findOne({ _id: id }, params);
+  const doc = await chainPopulate(Model.findOne({ _id: id }, params), Model);
   const modelName = getModelName(Model);
 
   if (!doc) {
@@ -16,7 +17,11 @@ export const getDoc = async (Model, id, params) => {
   return doc;
 };
 
-export const createDoc = (Model, data) => Model.create(data);
+export const createDoc = async (Model, data) => {
+  const newDoc = await Model.create(data);
+
+  return getDoc(Model, newDoc._id);
+};
 
 export const patchDoc = async (Model, id, data) => {
   const fieldsToPatch = {};
@@ -29,11 +34,14 @@ export const patchDoc = async (Model, id, data) => {
 
   await getDoc(Model, id);
 
-  return Model.findByIdAndUpdate(id, { $set: fieldsToPatch }, { new: true });
+  return chainPopulate(
+    Model.findByIdAndUpdate(id, { $set: fieldsToPatch }, { new: true }),
+    Model,
+  );
 };
 
 export const deleteDoc = async (Model, id) => {
   await getDoc(Model, id);
 
-  return Model.findByIdAndDelete(id);
+  return chainPopulate(Model.findByIdAndDelete(id), Model);
 };
